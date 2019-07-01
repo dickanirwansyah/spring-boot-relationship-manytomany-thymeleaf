4a5f63b348da8bd45ccd811c7d683207c6d7c05a
dickanirwansyah <dicka.nirwansyah@fusi24.com>
2019-07-01 09:39:24 +0700

    update

--- src/main/java/com/dicka/examplerelationshiph2dbthymeleaf/ExampleRelationshipH2dbThymeleafApplication.java
+++ src/main/java/com/dicka/examplerelationshiph2dbthymeleaf/ExampleRelationshipH2dbThymeleafApplication.java
@@ -29,30 +29,53 @@ class CommandLineRunnerData implements CommandLineRunner{
 	@Autowired
 	private StudentRepository studentRepository;
 
-	@Autowired
-	private CourseRepository courseRepository;
-
 	@Override
 	public void run(String... args) throws Exception {
 
-		//INSERT MANY TO MANY
-		String[] data = {"go", "php", "java"};
-		List<Course> courses = new ArrayList<>();
+		try{
 
-		Course course;
-		for (int i=0; i < data.length; i++){
-			course = new Course();
-			course.setCourserName(data[i]);
-			courses.add(course);
-			courseRepository.save(course);
+			List<Student> students = new ArrayList<>();
+			students = studentRepository.cariStudentBerdasarkanFirstname("dicka");
+
+			if (students.size() > 0){
+				System.out.println("found : "+students.size());
+			}else{
+				System.out.println("notfound.");
+			}
+
+		}catch (NullPointerException e){
+			e.printStackTrace();
 		}
 
-		Student student = new Student();
-		student.setFirstname("mumu");
-		student.setLastname("gomez");
-		student.setEmail("mumugomez@gmail.com");
-		student.getCourses().addAll(courses);
-		studentRepository.save(student);
-
 	}
+
+//	@Autowired
+//	private StudentRepository studentRepository;
+//
+//	@Autowired
+//	private CourseRepository courseRepository;
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//
+//		//INSERT MANY TO MANY
+//		String[] data = {"go", "php", "java"};
+//		List<Course> courses = new ArrayList<>();
+//
+//		Course course;
+//		for (int i=0; i < data.length; i++){
+//			course = new Course();
+//			course.setCourserName(data[i]);
+//			courses.add(course);
+//			courseRepository.save(course);
+//		}
+//
+//		Student student = new Student();
+//		student.setFirstname("mumu");
+//		student.setLastname("gomez");
+//		student.setEmail("mumugomez@gmail.com");
+//		student.getCourses().addAll(courses);
+//		studentRepository.save(student);
+//
+//	}
 }
--- src/main/java/com/dicka/examplerelationshiph2dbthymeleaf/repository/StudentRepository.java
+++ src/main/java/com/dicka/examplerelationshiph2dbthymeleaf/repository/StudentRepository.java
@@ -2,8 +2,15 @@ package com.dicka.examplerelationshiph2dbthymeleaf.repository;
 
 import com.dicka.examplerelationshiph2dbthymeleaf.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     Student findByStudentId(Long studentId);
+
+    @Query("SELECT s FROM Student s WHERE s.firstname = :firstname")
+    List<Student> cariStudentBerdasarkanFirstname(@Param(value = "firstname")String firstname);
 }
